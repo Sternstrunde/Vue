bc03244b1448e5c6b5681d68bccf093c11e0d4f3
郑龙龙 <10434542+zheng-longlong1@user.noreply.gitee.com>
2023-07-11 16:43:07 +0800

    ✨ feat: 代码规范的相关配置

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-import demo from './demo.vue';
+// import demo from './demo.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      
     </div>
   </header>
 
